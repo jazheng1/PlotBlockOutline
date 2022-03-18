a59d82e808a368f38bc3bec08e54f590010a99ba
jazheng <59896127+jazheng1@users.noreply.github.com>
2022-03-17 23:55:22 -0400

    python to C# convert

---
+++ PlotBlockOutline.cs
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import numpy as np
+
+public void plot_block_outline(block Block,ax axes3d) {
+  IMAX,JMAX,KMAX = block.X.shape;
+  X = block.X;
+  Y = block.Y;
+  Z = block.Z;
+
+  for (int i = 0; i < IMAX-1; i++) {    // Plots curves at constant I bounds
+    for (int j = 0; j < JMAX-1; j++) {
+      for(int k = 0; k < KMAX-1; k++) {
+        x = X[i,j,k];
+        y = Y[i,j,k];
+        z = Z[i,j,k];
+        ax.plot3D(x,y,z);
+    }
+  }
+  for (int j = 0; j < JMAX-1; j++){    // Plots curves at constant I bounds
+      for (int k = 0; k < KMAX-1; k++){
+        for (int i = 0; i < IMAX-1; i++) {
+          x = X[i,j,k];
+          y = Y[i,j,k];
+          z = Z[i,j,k];
+          ax.plot3D(x,y,z);
+        }
+      }
+  }
+  for (int i = 0; i < IMAX-1; i++){    // Plots curves at constant I bounds
+      for (int k = 0; k < KMAX-1; k++){
+        for(int j = 0; j < JMAX-1; j++) {
+          x = X[i,j,k];
+          y = Y[i,j,k];
+          z = Z[i,j,k];
+          ax.plot3D(x,y,z);
+        }
+      }
+  }
+}
